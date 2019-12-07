c5776ad32b83a41a4e4e11d777196b771b4b9730
Max Bisesi <max.bisesi@gmail.com>
2019-12-07 18:55:53 -0500

    progress is slow, css router issues

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,15 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-      rules: [
-          { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
-      ]
-  }
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/react', '@babel/env']
+        }
+      }
+    }]
+  },
 };
