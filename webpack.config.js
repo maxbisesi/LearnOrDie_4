185557803e7047310af6a9432586fbe1a97ca920
Max Bisesi <max.bisesi@gmail.com>
2019-12-08 14:03:51 -0500

    major progress on layout

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,34 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   resolve: {
-    modules: [path.resolve('./lib'), path.resolve('./node_modules')]
+    modules: [path.resolve("./lib"), path.resolve("./node_modules")]
   },
-  entry: ['babel-polyfill','./lib/renderers/domRender.js'],
-    output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+  entry: ["babel-polyfill", "./lib/renderers/domRender.js"],
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/react', '@babel/env']
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/react", "@babel/env"]
+          }
         }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-inline-loader"
       }
-    }]
-  },
+    ]
+  }
 };
