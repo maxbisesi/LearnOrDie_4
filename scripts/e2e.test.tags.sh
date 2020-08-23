40c3d0cfd3edb22f3cefb028497e891c2cc5e34f
Max Bisesi <max.bisesi@gmail.com>
2020-08-22 23:46:51 -0700

    browser shell arg good

---
+++ scripts/e2e.test.tags.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+browser="chrome"
+while getopts "b:fc" opt
+do
+   case "$opt" in
+      b ) browser="$OPTARG" ;;
+      f ) browser="firefox" ;;
+      c ) browser="chrome" ;;
+   esac
+done
+echo "Run UI tests in: $browser"
+# Set environment variables from .env and set NODE_ENV to test
+source <(dotenv-export | sed 's/\\n/\n/g')
+export TEST_BROWSER="$browser"
+#npx cucumber-js lib/cucumber/features --require-module @babel/register --require lib/Cucumber/steps --tags
